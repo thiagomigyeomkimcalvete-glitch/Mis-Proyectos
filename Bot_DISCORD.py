c9ec2d356f49b3bcb63b3887effe541ec0d59ab8
El Koreanito Facha <thiagomigyeomkimcalvete@gmail.com>
2026-05-03 20:47:06 -0300

    Update Bot_DISCORD.py
    Es un código para hacer un bot de Discord o para implementar partes de este código en tu propio bot de Discord

--- Bot_DISCORD.py
+++ Bot_DISCORD.py
@@ -1,3 +1,16 @@
+import discord
+from discord.ext import commands
+import random
+import os
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
@@ -29,4 +42,41 @@ async def password(ctx, longitud:int):
 
     await ctx.send("tu clave es:" + password)
 
-bot.run("PON TU PASSWORD AQUI")
+
+@bot.command()
+async def mem(ctx):
+    num = random.randint(1, 5)
+    images = os.listdir("images")
+    image = random.choice(images)
+
+    if num in (1, 2):
+        image_path = "images/Meme1.jpeg"
+    elif num in (3, 4):
+        image_path = "images/Chat_GPT_VS_Claude.jpg"
+    else:
+        images = os.listdir("images")
+        image_path = f"images/{random.choice(images)}"
+
+    with open(image_path, "rb") as f:
+        picture = discord.File(f)
+        await ctx.send(file=picture)
+
+    # Escribir en el archivo de texto
+    with open("text.txt", "a", encoding="utf-8") as f:
+        f.write("XD")
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+bot.run("PON TU PIN AQUI")
