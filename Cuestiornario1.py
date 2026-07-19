066b4e42df9dd0ee793337973095304c4c80aa25
El Koreanito Facha <thiagomigyeomkimcalvete@gmail.com>
2026-07-19 20:49:05 -0300

    Delete Cuestiornario1.py

--- Cuestiornario1.py
+++
@@ -1,7 +0,0 @@
-nombre = input("¿Cuál es tu nombre? ")
-edad = int(input("¿Cuántos años tienes? ")) 
-# Convertir la entrada del usuario a un número entero
-
-edad_el_proximo_ano = edad + 1
-
-print("Hola", nombre, ",en un año tendrás", edad_el_proximo_ano, "años")
